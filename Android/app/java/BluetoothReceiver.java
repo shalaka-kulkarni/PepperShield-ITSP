7a85fcfd7aa240dc83e124615b4bef99e322e46e
shalaka-kulkarni <shalaka-kulkarni@users.noreply.github.com>
2015-06-06 14:29:38 +0530

    Update BluetoothReceiver.java

--- Android/app/java/BluetoothReceiver.java
+++ Android/app/java/BluetoothReceiver.java
@@ -2,6 +2,7 @@ package com.mycompany.debugtrial2;
 
 import android.bluetooth.*;
 import android.graphics.*;
+import android.util.Log;
 
 import java.io.*;
 
@@ -16,39 +17,52 @@ public class BluetoothReceiver extends Thread {
 
     static int ctr = 1;
 
-
-    BluetoothReceiver(BluetoothSocket mSocket) {
+    public BluetoothReceiver(BluetoothSocket mSocket) {
         socket = mSocket;
         try {
             in = socket.getInputStream();
-        } catch (IOException e) { }
+            Log.d("BluetoothReceiver","Input stream received");
+        }
+        catch (IOException e) {
+            Log.d("BluetoothReceiver", "Input stream not received");
+        }
     }
-
     int read() {
 
-        int size=640*480*2 + 2; //640x480 pix screen resolution, with 1 pixel compressed to 2 bytes
+        int size=640*480 + 2; //640x480 pix screen resolution, with 1 pixel compressed to 3 bytes
         incomingArray = new byte[size];
         int noOfBytes = 0;
         try {
             noOfBytes = in.read(incomingArray);
-        } catch (IOException e) { }
+            Log.d("BluetoothReceiver", "Data received!");
+            Log.d("BluetoothReceiver", "Bytes read = "+noOfBytes);
+        } catch (IOException e) {
+            Log.d("BluetoothReceiver", "Data not received");
+        }
 
         return noOfBytes;
     }
 
     void getImage() {
 
-        Bitmap bitmapimage = BitmapFactory.decodeByteArray(incomingArray, 0, incomingArray.length);
-        String filepath = "/image"+ctr+".jpeg";
+        Bitmap bitmapImage = BitmapFactory.decodeByteArray(incomingArray, 0, incomingArray.length);
+        if(bitmapImage==null)
+            Log.d("BluetoothReceiver","Bitmap is null");
+        else
+            Log.d("BluetoothReceiver","Bitmap is not null");
+
+        String filepath = "/sdcard/bluetooth/image"+ctr+".jpeg";
         ctr++;
 
         try {
-            File imgfile = new File(filepath);
-            FileOutputStream fout = new FileOutputStream(imgfile);
-            bitmapimage.compress(Bitmap.CompressFormat.JPEG, 100, fout);
+            FileOutputStream fout = new FileOutputStream(filepath);
+            bitmapImage.compress(Bitmap.CompressFormat.JPEG, 100, fout);
             fout.flush();
             fout.close();
-        } catch (IOException e) { }
+            Log.d("BluetoothReceiver", "Image saved!");
+        } catch (IOException e) {
+            Log.d("BluetoothReceiver", "Image not saved");
+        }
 
     }
 }
