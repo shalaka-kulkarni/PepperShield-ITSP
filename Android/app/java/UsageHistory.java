652ebfe917ca615c14019836e2256e8ae11a339b
Abhin02 <Abhin02@users.noreply.github.com>
2015-06-08 14:07:16 +0530

    Update UsageHistory.java

--- Android/app/java/UsageHistory.java
+++ Android/app/java/UsageHistory.java
@@ -1,45 +1,66 @@
 package com.mycompany.peppershield;
 
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.content.Intent;
+import android.location.Location;
+import android.os.Bundle;
+import android.app.Activity;
+import android.os.ResultReceiver;
+import android.util.Log;
+import android.view.View;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.AdapterView;
+import android.widget.TextView;
 
-public class UsageHistory extends ActionBarActivity {
+
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+
+
+
+public class UsageHistory extends Activity
+{
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = getIntent();
         setContentView(R.layout.activity_usage_history);
-        getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        
+
+        String[] Contacts = {"Contact1", "Contact2", "Contact3"};
+        ListAdapter contactsAdapter = new CustomAdapter_UsageHistory(this, Contacts);
+        ListView ListView = (ListView) findViewById(R.id.ListView);
+        ListView.setAdapter(contactsAdapter);
+
+        ListView.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        String Contact = String.valueOf(parent.getItemAtPosition(position));
+//                        Toast.makeText(MainActivity.this,Contact,Toast.LENGTH_SHORT).show();
+                        Intent i = new Intent(UsageHistory.this, ContactInfo1_UsageHistory.class);
+                        i.putExtra("data", position + 1);
+                        startActivity(i);
+
+                    }
+                }
+        );
+
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_usage_history, menu);
-        return super.onCreateOptionsMenu(menu);
+
+
+
+
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        switch (item.getItemId()) {
-            case R.id.action_delete:
-                openDelete();
-                return true;
-            case R.id.action_settings:
-                openSettings();
-                return true;
-            case "Resend":
-                openResend();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-}
