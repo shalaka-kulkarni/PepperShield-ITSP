0c554205caec06b65951618289fab06f0c1d107a
Abhin02 <Abhin02@users.noreply.github.com>
2015-06-08 18:13:22 +0530

    Update MessageSender.java

--- Android/app/java/MessageSender.java
+++ Android/app/java/MessageSender.java
@@ -1,23 +1,27 @@
 package com.mycompany.debugtrial2;
 
-import android.content.ComponentName;
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.telephony.SmsManager;
-import android.util.Log;
 
 import java.io.File;
-import java.util.List;
 
 /**
  * Created by Shalaka on 5/25/2015.
  */
 public class MessageSender {
 
-    String emergency[][] = {/*{"Aai", "+91 9619745270"},*/ {"Shalaka", "+91 7506438504"}};
+    String contacts[][];
+    MessageSender()
+    {
+        EmergencyContacts init=new EmergencyContacts();
+       init.updateList();
+        for (int i=0;i<init.EmergencyContactsName.size();i++)
+        {   EmergencyContacts.Person p=init.EmergencyContactsName.get(i);
+            contacts[i][0]=p.name;
+            contacts[i][1]=p.phoneNumber;
+        }
+    }
 
     void sendSMS() {
 
@@ -25,8 +29,8 @@ public class MessageSender {
 
         SmsManager smsMgr = SmsManager.getDefault();
 
-        for(int i=0;i<emergency.length;i++) {
-            smsMgr.sendTextMessage(emergency[i][1], null, message, null, null);
+        for(int i=0;i< contacts.length;i++) {
+            smsMgr.sendTextMessage(contacts[i][1], null, message, null, null);
         }
 
     }
@@ -41,7 +45,6 @@ public class MessageSender {
         for (int i = 0; i < resInfo.size(); i++) {
             ResolveInfo ri = resInfo.get(i);
             String packageName = ri.activityInfo.packageName;
-
             if(packageName.contains("Messaging")) {
                 Log.d("TAG", packageName + " : " + ri.activityInfo.name);
                 sendIntent.setComponent(new ComponentName(packageName, ri.activityInfo.name));
@@ -51,7 +54,7 @@ public class MessageSender {
 
         Uri uri = Uri.fromFile(new File("/sdcard/bluetooth/test.jpg"));
 
-        sendIntent.putExtra("address", emergency[i][1]);
+        sendIntent.putExtra("address", contacts[i][1]);
         sendIntent.putExtra("sms_body", message);
         sendIntent.putExtra(Intent.EXTRA_STREAM, uri);
         sendIntent.setType("image/jpg");
