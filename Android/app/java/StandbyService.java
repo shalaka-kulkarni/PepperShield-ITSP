6640219c19eb0c3a67c44c2327f74e9f1abd0487
shalaka-kulkarni <shalaka-kulkarni@users.noreply.github.com>
2015-06-09 19:15:36 +0530

    Update StandbyService.java

--- Android/app/java/StandbyService.java
+++ Android/app/java/StandbyService.java
@@ -26,8 +26,21 @@ public class StandbyService extends Service {
         Log.d("StandbyService", "Service started");
         Bluetooth bt = new Bluetooth();
 
-        bt.run();
-        sendImage();
+        //bt.run();
+
+        boolean flag = false;
+        while(!flag) {
+            bt.connectAsClient();
+            boolean trigger = bt.runBT();
+            if (trigger) {
+                MessageSender sms = new MessageSender();
+                // sms.sendSMS();
+                Log.d("Bluetooth", "Message sent");
+                //sendImage();
+                Log.d("Bluetooth", "MMS initiated");
+                flag = true;
+            }
+        }
 
         return START_STICKY;
     }
@@ -36,11 +49,12 @@ public class StandbyService extends Service {
 
         MessageSender mms = new MessageSender();
 
-        for(int i=0;i<mms.emergency.length;i++) {
-            Intent sendIntent = mms.getMMSIntent(i);
+        for(int i=0;i<mms.contacts.length;i++) {
+            int ctr = new BluetoothReceiver().getCounter();
+            ctr -- ;
+            Intent sendIntent = mms.getMMSIntent(i, ctr);
             getApplicationContext().startActivity(sendIntent);
         }
-        Log.d("Bluetooth", "MMS sent");
     }
 
 }
