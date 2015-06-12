231e4808cbcf46e3b47503a87aac224abfbda301
Abhin02 <Abhin02@users.noreply.github.com>
2015-06-12 17:03:38 +0530

    Update StandbyService.java

--- Android/app/java/StandbyService.java
+++ Android/app/java/StandbyService.java
@@ -3,17 +3,24 @@ package com.mycompany.debugtrial2;
 import android.app.Service;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.os.IBinder;
+import android.preference.PreferenceManager;
 import android.util.Log;
+import android.widget.TextView;
 
 public class StandbyService extends Service {
     public StandbyService() {
     }
 
+
     @Override
-    public IBinder onBind(Intent intent) {
+    public IBinder onBind(Intent intent)
+    {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
+
     }
 
     @Override
@@ -33,15 +40,23 @@ public class StandbyService extends Service {
             bt.connectAsClient();
             boolean trigger = bt.runBT();
             if (trigger) {
-                /*MessageSender sms = new MessageSender();
-                sms.sendSMS();
-                Log.d("Bluetooth", "Message sent");*/
-                sendImage();
+                MessageSender sms = new MessageSender();
+                // sms.sendSMS();
+                Log.d("Bluetooth", "Message sent");
+                //sendImage();
                 Log.d("Bluetooth", "MMS initiated");
                 flag = true;
+                ContactInfo1_UsageHistory getPut=null;
+                StringBuilder Date=getPut.getDate();
+                StringBuilder Time= getPut.getTime();
+                SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+                SharedPreferences.Editor editor = sharedPref.edit();
+                int ctr = new BluetoothReceiver().getCounter();
+                editor.putString("Date"+ctr+"", Date.toString());
+                editor.putString("Time"+ctr+"", Time.toString());
+                editor.apply();
             }
         }
-
         return START_STICKY;
     }
 
