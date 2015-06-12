790d3de7bb0a51fa95a9fcb13a73eac74808d266
Abhin02 <Abhin02@users.noreply.github.com>
2015-06-12 17:02:41 +0530

    Update UsageHistory.java

--- Android/app/java/UsageHistory.java
+++ Android/app/java/UsageHistory.java
@@ -2,19 +2,26 @@ package com.mycompany.peppershield;
 
 import android.content.Intent;
 import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.os.Environment;
 import android.os.ResultReceiver;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.AdapterView;
 import android.widget.TextView;
 
 
-
+import java.io.File;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Locale;
 
@@ -24,19 +31,26 @@ import java.util.Locale;
 public class UsageHistory extends Activity
 {
 
+    ArrayList<String> Contacts;
 
-
-
+    CustomAdapter_UsageHistory contactsAdapter = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_usage_history);
+        Contacts = new ArrayList<>();
+ //       int ctr = new BluetoothReceiver().getCounter();
+      int ctr=5;
+        if (ctr > 1)
+        {
+            for (int i = 0; i < ctr; i++)
+            {
+                Contacts.add("Contacts" + i + "") ;
+            }
+        }
 
-        
-
-        String[] Contacts = {"Contact1", "Contact2", "Contact3"};
-        ListAdapter contactsAdapter = new CustomAdapter_UsageHistory(this, Contacts);
+        contactsAdapter = new CustomAdapter_UsageHistory();
         ListView ListView = (ListView) findViewById(R.id.ListView);
         ListView.setAdapter(contactsAdapter);
 
@@ -57,10 +71,53 @@ public class UsageHistory extends Activity
 
 
     }
+    class CustomAdapter_UsageHistory extends ArrayAdapter<String> {
 
+        public CustomAdapter_UsageHistory()
+        {
+            super(UsageHistory.this, android.R.layout.simple_list_item_1,Contacts);
+        }
 
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater abhinInflater=LayoutInflater.from(getContext());
+            View customView=abhinInflater.inflate(R.layout.custom_row_usagehistory, parent, false);
 
+//         String  singleContactItem=getItem(position);
+            TextView textView=(TextView)customView.findViewById(R.id.textView);
+//         EditText editText=(EditText)customView.findViewById(R.id.editText);
+            ImageView Image=(ImageView) customView.findViewById(R.id.Image);
+            File myDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "PepperShield");
+            myDir.mkdirs();
+            File file = new File(myDir,"image"+position+".jpeg");
+            Uri uri = Uri.fromFile(file);
+            Image.setImageURI(uri);
+//        Text.setText(singleContactItem);
 
+//         if (position==0) {
+//
+//             Image.setImageURI(Uri.parse(new File("/sdcard/bluetooth/image"+position+".jpeg").toString()));
+//
+////             Image.setImageResource(R.drawable.kohli);
+//         }
+//         else if(position==1)
+//         {
+////             Image.setImageResource(R.drawable.yuvi);
+//             Image.setImageURI(Uri.parse(new File("/sdcard/bluetooth/image"+position+".jpeg").toString()));
+//         }
+//         else if (position==2)
+//         {
+////             Image.setImageResource(R.drawable.sreesanth);
+//             Image.setImageURI(Uri.parse(new File("/sdcard/bluetooth/image"+position+".jpeg").toString()));
+//
+//         }
+            return customView;
 
+        }
     }
 
+
+
+
+
+}
